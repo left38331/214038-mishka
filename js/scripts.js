3de00bd85170c06339b1c2128a606fd011801ba3
left38331 <left38331@mail.ru>
2016-12-24 22:14:27 +0300

    устранение правок после проверки наставником

--- js/scripts.js
+++ js/scripts.js
@@ -2,23 +2,6 @@
     var navUser = document.querySelector(".user-nav");
     var navToggle = document.querySelector('.page-header__toggle');
 
-    var link = document.querySelector(".product__buy");
-    var popup = document.querySelector(".modal-content");
-    var close = popup.querySelector(".modal-content-close");
-    var overlay = document.querySelector(".modal-overlay");
-
-    link.addEventListener("click", function(event) {
-      event.preventDefault();
-      popup.classList.add("modal-content-show");
-      overlay.classList.add("modal-overlay-show");
-    });
-
-    overlay.addEventListener("click", function(event) {
-      event.preventDefault();
-      popup.classList.remove("modal-content-show");
-      overlay.classList.remove("modal-overlay-show");
-    });
-
     navMain.classList.remove('main-nav--nojs');
     navUser.classList.remove('user-nav--nojs');
     navToggle.classList.remove("page-header__toggle--nojs");
@@ -41,4 +24,21 @@
         navUser.classList.add("user-nav--closed");
         navUser.classList.remove("user-nav--opened");
       }
+    });
+
+    var link = document.querySelector(".product__buy");
+    var popup = document.querySelector(".modal-content");
+    var close = document.querySelector(".modal-content-close");
+    var overlay = document.querySelector(".modal-overlay");
+
+    link.addEventListener("click", function(event) {
+      event.preventDefault();
+      popup.classList.add("modal-content-show");
+      overlay.classList.add("modal-overlay-show");
+    });
+
+    overlay.addEventListener("click", function(event) {
+      event.preventDefault();
+      popup.classList.remove("modal-content-show");
+      overlay.classList.remove("modal-overlay-show");
     });
